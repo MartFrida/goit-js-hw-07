35de3533a0a8940099e599a63a1e619c7242a7b8
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-03 18:21:15 +0100

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('ul.gallery');
+
+function createMarkUp({ preview, original, description }) {
+  return `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+}
+
+const galaryCartArray = galleryItems.map(item => createMarkUp(item));
+console.log(galaryCartArray);
+
+galleryList.insertAdjacentHTML('afterbegin', galaryCartArray.join(''));
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
